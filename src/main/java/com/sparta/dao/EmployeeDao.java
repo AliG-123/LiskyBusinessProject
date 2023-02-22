d95eb0a5f550bc771e920adc611b0073d31a8563
HMalik <hmalik-@hotmail.com>
2023-02-22 16:52:55 +0000

    removed changes

--- src/main/java/com/sparta/dao/EmployeeDao.java
+++ src/main/java/com/sparta/dao/EmployeeDao.java
@@ -1,5 +1,23 @@
 package com.sparta.dao;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class EmployeeDao {
+
+
+    private Connection connection;
+
+    public EmployeeDao() throws SQLException {
+
+    }
+
+
+
 
 }
