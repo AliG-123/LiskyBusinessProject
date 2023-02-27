6616b4e574021dfa5039af2393e56037a868fc2b
Miguel <miguel.camilleri1@gmail.com>
2023-02-27 12:32:50 +0000

    Implemented date past checker and created a test class

--- src/main/java/com/sparta/validation/date_past_checker.java
+++ src/main/java/com/sparta/validation/date_past_checker.java
@@ -1,4 +1,19 @@
 package com.sparta.validation;
 
+import com.sparta.entities.Employee;
+
+import java.sql.Date;
+
 public class date_past_checker {
+
+    public boolean check(Employee employee){
+        boolean isPast = false;
+        Date currentDate = new Date(System.currentTimeMillis());
+        if(employee.getBirth_date().before(currentDate) && employee.getHire_date().before(currentDate)){
+            isPast = true;
+        }
+
+        return isPast;
+
+    }
 }
---
+++ src/test/java/com/sparta/validation/date_past_checkerTest.java
@@ -0,0 +1,7 @@
+package com.sparta.validation;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class date_past_checkerTest {
+
+}
